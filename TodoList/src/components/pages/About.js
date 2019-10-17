cd3693227baada57e54fe6c4dcdcacccfbd6b9b6
Anmol Sandhu <35971743+assandhu23@users.noreply.github.com>
2019-10-17 12:25:15 -0400

    Add files via upload

---
+++ TodoList/src/components/pages/About.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function About() {
+    return (
+        <React.Fragment>
+            <h1>
+                About
+            </h1>
+            <p>
+                This is TodoList app.
+                Used basic react features like
+                components, state, props, JSX, events, forms etc.             </p>
+        </React.Fragment>
+    )
+}
+export default About;
